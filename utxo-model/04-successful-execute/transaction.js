de440acb3ea282c7fe927f8ae52fdd3960d9d917
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-02 16:27:08 +0500

    add 04-successful-execute challenge code

---
+++ utxo-model/04-successful-execute/transaction.js
@@ -0,0 +1,39 @@
+class Transaction {
+    constructor(inputUTXOs, outputUTXOs) {
+        this.inputUTXOs = inputUTXOs;
+        this.outputUTXOs = outputUTXOs;
+    }
+    execute() {
+        let inputTotalValue = 0;
+        let outputTotalValue = 0;
+
+        this.inputUTXOs.forEach((input) => {
+            // * get the spent and amount properties from input object.
+            const { spent, amount } = input;
+            inputTotalValue += amount;
+            // * check whether it is true or not, if it is true then throw error.
+            if (spent) {
+                throw "already spent";
+            }
+        });
+
+        this.outputUTXOs.forEach((output) => {
+            const { amount } = output;
+            outputTotalValue += amount;
+        });
+
+        if (inputTotalValue < outputTotalValue) {
+            throw "is less than";
+        }
+
+        this.inputUTXOs.forEach((input) => {
+            input.spend();
+        });
+
+        this.outputUTXOs.forEach((output) => {
+            output.spend();
+        });
+    }
+}
+
+module.exports = Transaction;
