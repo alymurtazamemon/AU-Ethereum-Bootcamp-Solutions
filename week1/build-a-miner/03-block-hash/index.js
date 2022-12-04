269e7e9ee2d0a3939b285a4ecc8d4c09ca559c51
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-04 15:17:44 +0500

    add week 1 03-block-hash challenge code

---
+++ week1/build-a-miner/03-block-hash/index.js
@@ -0,0 +1,31 @@
+const SHA256 = require("crypto-js/sha256");
+const TARGET_DIFFICULTY =
+    BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const MAX_TRANSACTIONS = 10;
+
+const mempool = [];
+const blocks = [];
+
+function addTransaction(transaction) {
+    mempool.push(transaction);
+}
+
+function mine() {
+    const blockObj = {
+        id: blocks.length
+    };
+
+    const stringifyBlockObj = SHA256(JSON.stringify(blockObj));
+    blockObj.hash = stringifyBlockObj;
+
+    blocks.push(blockObj);
+}
+
+module.exports = {
+    TARGET_DIFFICULTY,
+    MAX_TRANSACTIONS,
+    addTransaction,
+    mine,
+    blocks,
+    mempool,
+};
