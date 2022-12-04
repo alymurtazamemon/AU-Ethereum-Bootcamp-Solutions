52badf483b9eac67d271b092d2cfd48308ee9a36
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-04 15:15:22 +0500

    add week 1 02-mine-block challenge code

---
+++ week1/build-a-miner/02-mine-block/index.js
@@ -0,0 +1,28 @@
+const SHA256 = require("crypto-js/sha256");
+const TARGET_DIFFICULTY =
+    BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const MAX_TRANSACTIONS = 10;
+
+const mempool = [];
+const blocks = [];
+
+function addTransaction(transaction) {
+    mempool.push(transaction);
+}
+
+function mine() {
+    const blockObj = {
+        id: blocks.length
+    };
+
+    blocks.push(blockObj);
+}
+
+module.exports = {
+    TARGET_DIFFICULTY,
+    MAX_TRANSACTIONS,
+    addTransaction,
+    mine,
+    blocks,
+    mempool,
+};
