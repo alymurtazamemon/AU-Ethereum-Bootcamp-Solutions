e7ab94d8e81016c8181b4e1f5ff78c87f9482d8e
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-16 18:52:07 +0500

    add 04-string-literals challenge code

---
+++ week4/basic-data-types/04-string-literals/contract.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Contract {
+    bytes32 public msg1 = "Hello World";
+    string public msg2 = "This string is more than 32 bytes";
+}
