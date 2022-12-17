4b0e9ef0927c7985dc0ad2b43231dbd5ec38f328
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-17 10:54:56 +0500

    add 04-charity challenge code

---
+++ week4/sending-ether/04-charity/contract.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Contract {
+    address public owner;
+    address public charityAddress;
+
+    constructor(address _charityAddress) {
+        owner = msg.sender;
+        charityAddress = _charityAddress;
+    }
+
+    receive() external payable {}
+
+    function tip() public payable {
+        (bool success, ) = owner.call{value: msg.value}("");
+        require(success);
+    }
+
+    function donate() external {
+        (bool success, ) = charityAddress.call{value: address(this).balance}(
+            ""
+        );
+        require(success);
+    }
+}
