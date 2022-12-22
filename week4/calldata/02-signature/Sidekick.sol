69431e8a147f3d6f6d09155be9638c82c8654a28
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-22 14:09:20 +0500

    add 02-signature challenge code

---
+++ week4/calldata/02-signature/Sidekick.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Sidekick {
+    function sendAlert(address hero) external {
+        bytes4 signature = bytes4(keccak256("alert()"));
+
+        (bool success, ) = hero.call(abi.encodePacked(signature));
+
+        require(success);
+    }
+}
