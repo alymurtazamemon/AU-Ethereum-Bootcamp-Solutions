9cbd4b0bdc6442741a5afa014d8933da87224cd9
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-23 12:19:52 +0500

    add 03-remove-member challenge code

---
+++ week5/mappings/03-remove-member/contract.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Contract {
+    mapping(address => bool) public members;
+
+    function addMember(address newMember) external {
+        members[newMember] = true;
+    }
+
+    function isMember(address member) external view returns (bool) {
+        return members[member];
+    }
+
+    function removeMember(address member) external {
+        members[member] = false;
+    }
+}
