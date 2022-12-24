a9eca8ddea40728bd66284df641fbbc963e7eac7
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-24 15:17:18 +0500

    add 02-vote-memory challenge code

---
+++ week5/structs/02-vote-memory/contract.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+pragma experimental ABIEncoderV2;
+
+contract Contract {
+    enum Choices {
+        Yes,
+        No
+    }
+
+    struct Vote {
+        Choices choice;
+        address voter;
+    }
+
+    function createVote(Choices choice) external view returns (Vote memory) {
+        return Vote(choice, msg.sender);
+    }
+}
