88a2b26d688dcb7d4f20843834143b54f5511fbb
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-24 08:15:35 +0500

    add 04-approval challenge code

---
+++ week5/escrow/04-approval/escrow.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Escrow {
+    address public depositor;
+    address public beneficiary;
+    address public arbiter;
+    bool public isApproved;
+
+    constructor(address _arbiter, address _beneficiary) payable {
+        depositor = msg.sender;
+        beneficiary = _beneficiary;
+        arbiter = _arbiter;
+    }
+
+    function approve() external {
+        isApproved = true;
+        (bool success, ) = beneficiary.call{value: address(this).balance}("");
+        require(success);
+    }
+}
