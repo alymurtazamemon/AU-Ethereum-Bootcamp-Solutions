0abef4a4df7b3c8739cd75dcd7a12786105a0b41
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-01-09 16:41:46 +0500

    add 01-rsvp challenge code

---
+++ week6/party-split/01-rsvp/Party.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+error Party__AlreadyRegistered();
+error Party__InvalidAmount();
+
+contract Party {
+	uint private rsvpAmount;
+    
+    address[] private addresses; 
+
+    constructor(uint amount) {
+        rsvpAmount = amount;
+    }
+
+    function rsvp() external payable {
+        address[] memory addressesCopy = addresses;
+        bool isTrue;
+
+        for(uint i = 0; i < addressesCopy.length; i++) {
+            if(addressesCopy[i] == msg.sender) {
+                isTrue = true;
+                break;
+            }    
+        }
+
+        if(isTrue) {
+            revert Party__AlreadyRegistered();
+        }
+
+        if(msg.value < rsvpAmount || msg.value > rsvpAmount) {
+            revert Party__InvalidAmount();
+        }
+
+        addresses.push(msg.sender);
+    }
+}
