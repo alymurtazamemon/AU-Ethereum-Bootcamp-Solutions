212bfeaddc5638f54a84e256db81c887825a4fc4
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-01-10 12:54:47 +0500

    add 02-configuration challenge code

---
+++ week6/build-an-erc-20/02-configuration/Token.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract Token {
+    uint public totalSupply;
+    uint8 public decimals = 18;
+    string public name = "Rupee";
+    string public symbol = "PKR";
+}
