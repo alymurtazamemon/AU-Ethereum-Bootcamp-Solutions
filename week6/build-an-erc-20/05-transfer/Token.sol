7928d5cb0e5478ffad886aa190ac75005601a4fd
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-01-10 20:56:39 +0500

    add 05-transfer challenge code

---
+++ week6/build-an-erc-20/05-transfer/Token.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+error Token__NotEnoughFunds();
+
+contract Token {
+    uint public totalSupply;
+    uint8 public decimals = 18;
+    string public name = "Rupee";
+    string public symbol = "PKR";
+
+    mapping(address => uint256) private _balances;
+
+    constructor() {
+        totalSupply = 1000 * 1e18;
+        _balances[msg.sender] = totalSupply;
+    }
+
+    function balanceOf(address owner) external view returns(uint256) {
+        return _balances[owner];
+    }
+
+    function transfer(address recipient, uint amount) public returns(bool){
+        if(_balances[msg.sender] < amount) {
+            revert Token__NotEnoughFunds();
+        }
+
+        _balances[msg.sender] -= amount;
+        _balances[recipient] += amount;
+
+        return true;
+    }
+}
