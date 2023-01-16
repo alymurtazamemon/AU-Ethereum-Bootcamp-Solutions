cffc1f1a2a7f6a4bf0e9975cd420704f7b0823a6
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-01-16 10:30:12 +0500

    add 03-multiple-votes challenge code.

---
+++ week7/voting-contract/03-multiple-votes/Voting.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Voting {
+    struct Proposal {
+        address target;
+        bytes data;
+        uint yesCount;
+        uint noCount;
+    }
+    
+    Proposal[] public proposals;
+
+    mapping(uint => bool) private proposalIdToVoteStatus;
+    
+    function newProposal(address target, bytes memory data) external {
+        proposals.push(Proposal(target, data, 0 , 0));
+    }
+
+    function castVote(uint proposalId, bool status) external {
+        Proposal storage proposal = proposals[proposalId];
+
+        // * check whether user cast the vote before?
+        if(proposalIdToVoteStatus[proposalId]) {
+            // * true means casted.
+
+            if(status) {
+                proposal.yesCount++;
+                if(proposal.noCount > 0) {
+                    proposal.noCount--;
+                }
+            } else {
+                proposal.noCount++;
+                if(proposal.yesCount > 0) {
+                    proposal.yesCount--;
+                }
+            }
+        } else {
+            // * false means not casted.
+
+            if(status) {
+                proposal.yesCount++;
+            } else {
+                proposal.noCount++;
+            }
+
+            proposalIdToVoteStatus[proposalId] = true;
+        }
+    }
+}
